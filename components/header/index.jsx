618dfdc7e73034adc6c7160d5363c0380092613a
paulo.reducino <paulo.reducino@phooto.com.br>
2024-04-19 21:55:42 -0300

    atualizando button no header

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link"
 import Logo from "/public/images/logo.webp";
 import Styles from "./header.module.scss";
+import Button from "../button";
 
 
 const Header = () => {
@@ -16,8 +17,8 @@ const Header = () => {
             <Link href="/">Catálogo</Link>
             <Link href="/">Sobre nós</Link>
         </div>
-        <div className={Styles.action}>
-        <button className={Styles.button}>Fale conosco</button>
+        <div className={Styles.action}>        
+        <Button title="Fale conosco" />
         </div>
     </div>
     );
