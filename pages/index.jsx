bc74a12925129960f2bd0d01ec73c202e7a8d578
paulo.reducino <paulo.reducino@phooto.com.br>
2024-04-19 21:56:57 -0300

    criando sessão welcome

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,13 @@
 import { Inter } from "next/font/google";
 import Header from "@/components/header";
 import Styles from "./Home.module.scss"
+import Welcome from "@/components/Welcome";
 
 export default function Home() {
  return (
  <div className={Styles.container}>
     <Header />
+    <Welcome />
  </div>
  );
 }
